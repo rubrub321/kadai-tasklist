6a795aaf33678340f86606fbccd444b49dd75037
rub.rub <rrub408@gmail.com>
2020-06-26 07:14:45 +0000

    add user new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+ 
   root to: 'tasks#index'
   
   resources :tasks
-  resources :users, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 end
